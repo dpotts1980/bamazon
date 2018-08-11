fc8744e3baceceb0923087c84ea4f1f708c06d76
Daniel Potts <drp777@hotmail.com>
2018-08-11 09:35:31 -0500

    added show merchandise function and displayed items to the command line

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,6 +17,36 @@ var connection = mysql.createConnection({
   connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    connection.end();
+    //connection.end();
+    showMerchandise();
   });
+ 
+
+  function showMerchandise() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        if(err) throw err
+         for (let i = 0; i < res.length; i++) {
+            console.log(" - - - - - - - - - - - - - - - ")
+            console.log("item number: " + res[i].item_id)
+            console.log("item: " + res[i].product_name)
+            console.log("price: $" + res[i].price)
+        }//end of for loop
+    });//end of query
+
+  }//end of showMerchandise
+  showMerchandise();
+ /*  function readProducts() {
+    console.log("Selecting all products...\n");
+    connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+      // Log all results of the SELECT statement
+      console.log(res);
+      connection.end();
+    });
+  }
+  readProducts(); */
   
+  // function which prompts the user for what action they should take
+/* function start() {
+
+} *///end of function start
