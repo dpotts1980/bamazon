0345458affc23cd9fadc95de23bce1c561de1658
Daniel Potts <daniel.potts@seiuhcil.org>
2018-08-16 17:46:58 -0500

    got response on quantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -34,15 +34,45 @@ function showMerchandise() {
   function purchase() {
     console.log("arr", arr)
     inquirer
-      .prompt({
-        name: "itemID",
+      .prompt([
+        {
+        name: "id",
         type: "input",
         message: "What is the item ID you would like to purchase? ",
-        choices: arr
-      }).then(function(ans){
-        console.log(ans)
-        checkItemamount(res.itemID)
-        //call to another function that 
-      })
+        
+        },
+        {
+        name: "stock_quantity",
+        type: "input",
+        message: "How many would you like to purchase? ",
+        
+      }
+      ])
+        
+      .then(function(ans){
+        var item = ans.id;
+        var quantity = ans.stock_quantity;
+        console.log(ans.id);
+        console.log(ans.stock_quantity);
+         connection.query("SELECT * FROM products", function(err,res) {
+          if (err) throw err;
+          if (res.length === 0) {
+            console.log("ERROR: Invalid Item. Please select a valid ID.")
+            showMerchandise();
+           } else {
+            var itemInfo = res[0];
+            if (quantity <= itemInfo.stock_quantity) {
+              console.log(itemInfo.product_name + "is available! Let me place your order now! ")
+            }
+          }
+             
+          })//end of connection.query
+        //end of then(function(ans))
+
+        
+        
+        
+      })//end of .then function
   }//end of function purchase
   
+ 
