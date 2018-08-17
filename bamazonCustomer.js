d6e9e6c6b7631fb35bbe0bd5d73306eb5029baa1
Daniel Potts <drp777@hotmail.com>
2018-08-17 13:20:17 -0500

    created check inventory and how much

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,9 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var table = require("console.table")
+var table = require("console.table");
+
+var amountDue;
+var department;
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -12,11 +15,14 @@ var connection = mysql.createConnection({
     password: "password",
     database: "bamazonDB"
   });
-  
+ 
+ 
+
   connection.connect(function(err) {
     if (err) throw err;
     });
  var arr = [];
+ //this is the function that shows available merchandise//
 function showMerchandise() {
     connection.query("SELECT * FROM products", function(err, res) {
     for (let r of res){
@@ -37,42 +43,55 @@ function showMerchandise() {
       .prompt([
         {
         name: "id",
-        type: "input",
+        //type: "input",
         message: "What is the item ID you would like to purchase? ",
+        validate: function(value){
+          var valid = value.match(/^[0-9]+$/)
+          if(valid){
+            return true
+          }
+            return 'Please enter a valid Product ID'
+        }
         
         },
         {
         name: "stock_quantity",
-        type: "input",
+        //type: "input",
         message: "How many would you like to purchase? ",
-        
+        validate: function(value){
+          var valid = value.match(/^[0-9]+$/)
+          if(valid){
+            return true
+          }
+            return 'Please enter a numerical value'
+        }
       }
       ])
         
       .then(function(ans){
-        var item = ans.id;
-        var quantity = ans.stock_quantity;
-        console.log(ans.id);
-        console.log(ans.stock_quantity);
-         connection.query("SELECT * FROM products", function(err,res) {
-          if (err) throw err;
-          if (res.length === 0) {
-            console.log("ERROR: Invalid Item. Please select a valid ID.")
-            showMerchandise();
-           } else {
-            var itemInfo = res[0];
-            if (quantity <= itemInfo.stock_quantity) {
-              console.log(itemInfo.product_name + "is available! Let me place your order now! ")
-            }
-          }
-             
-          })//end of connection.query
-        //end of then(function(ans))
+       /*  var item = ans.id; 
+        var quantity = ans.stock_quantity; */
+       // console.log(ans.id);
+        //console.log(ans.stock_quantity);
+         connection.query("SELECT * FROM products WHERE id = ?", [ans.id], function(err,res) {
+          if(ans.stock_quantity > res[0].stock_quantity) {
+            console.log('Insufficient Quantity');
+		      	console.log('This order has been cancelled');
+			      console.log('');
+			      newPurchase();
+    }
+          else{
+            amountDue = res[0].price * ans.stock_quantity;
+            department = res[0].department_name;
+            console.log('Thanks for your order');
+			      console.log('You owe $' + amountDue);
+            console.log('');
+            //here is where we'll update our inventory
 
+          }
+          });
+        });
         
-        
-        
-      })//end of .then function
   }//end of function purchase
   
  
